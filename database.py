dae2937a30c5a323ae5a4fe80900c8314d756c13
Margaret Mwangi <margaret.mwangi3@moringaschool.com>
2024-09-19 14:07:42 +0300

    set up my database to fetch from database

--- database.py
+++ database.py
@@ -1,1 +1,27 @@
-DATABASE_URL = "sqlite:///./books.db"
+import sqlite3
+
+DATABASE = "books.db"
+
+
+def get_connection():
+    ''' get a new connection to the database.'''
+    conn = sqlite3.connect(DATABASE)
+    return conn
+
+
+def execute_query(query, params=()):
+    ''' execute a query on the database and return the results'''
+    with get_connection() as conn:
+        Cursor = conn.cursor()
+        Cursor.execute(query, params)
+        conn.commit()
+        
+        
+def fetch_query(query, params=()):
+    ''' execute a query on the database and return the results'''
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        return cursor.fetchall()
+    
+
